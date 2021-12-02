65be9d5553a6bf814f2d273496408056369ee5d5
frame88 <fleuzzi97@gmail.com>
2021-12-02 19:11:42 +0100

    doffolta

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
 //primo giorno: fare la parte grafica
 //secondo giorno: fare la parte javascript
+const play = document.querySelector('.play');
+const grid = document.getElementById('grid');
+const select = document.getElementById ('level');
+let numin = 1;
+let numax = 100;
 
-let play = document.querySelector('.play');
-let grid = document.querySelector('grid');
+//cambio di livello
+
+select.addEventListener('change', function(){
+    console.log('inizio gioco');
+    let level = select.value;
+    console.log('scelta livello', level);
+
+    changelvl(level, numax);
+});
+
+//in base al livello scelgo il numero di celle
+
+function changelvl(lvl, numCells) {
+    if (lvl == 1) {
+        numCells = 49;
+    }
+    else if (lvl == 2) {
+        numCells = 81;
+    }
+    else {
+        numCells == 100;
+    }
+    console.log(numCells);
+    return numCells;
+}
